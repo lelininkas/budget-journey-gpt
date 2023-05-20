36e236f1d269c95e5ceabd826386a210d0a1f3c6
Edvinas Beinartas <edvinas.beinartas@gmail.com>
2023-05-20 05:12:45 +0300

    Add: jpa, dtos, service, and main view

--- src/main/java/com/lelininkas/budgetjourney/BudgetJourneyApplication.java
+++ src/main/java/com/lelininkas/budgetjourney/BudgetJourneyApplication.java
@@ -1,10 +1,21 @@
 package com.lelininkas.budgetjourney;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
 @SpringBootApplication
-public class BudgetJourneyApplication {
+@Theme(value = "TripOnBudget", variant = Lumo.DARK)
+public class BudgetJourneyApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetJourneyApplication.class, args);
