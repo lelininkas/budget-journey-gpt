b7fa910ff03b1303584614675e4046ce17d3f804
Edvinas Beinartas <edvinas.beinartas@gmail.com>
2023-05-20 04:06:26 +0300

    Add project stub

---
+++ src/main/java/com/lelininkas/budgetjourney/BudgetJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.lelininkas.budgetjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lelininkas/budgetjourney/BudgetJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lelininkas.budgetjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
